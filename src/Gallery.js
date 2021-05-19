9378f6f023a00aaef205c0df78dd375953a58e45
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-05-19 14:36:38 +0200

    .

---
+++ src/Gallery.js
@@ -0,0 +1,10 @@
+import "./Gallery.css";
+
+export default function showGallery(){
+    return(
+        <div className="Gallery"><section><h4>Dai un occhiata ad alcuni dei miei lavori recenti:</h4>
+        </section>
+        
+        </div>
+    )
+}
---
+++ src/Info.js
@@ -0,0 +1,10 @@
+import "./Info.css";
+
+export default function displayInfo(){return (
+    <div className="Info">
+        <section>
+           <h2>Su di me</h2> 
+           <p>.......</p>
+        </section>
+    </div>);
+}
