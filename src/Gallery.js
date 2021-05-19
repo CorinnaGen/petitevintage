388d3725404f7f1545ca0d9e9233190f4ed31a33
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-05-19 16:12:11 +0200

    img

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,10 +1,11 @@
 import "./Gallery.css";
+import img1 from "./img1.jpeg";
 
 
 export default function showGallery(){
     return(<div className="row">
         <div className="col-4"><section><h4>Ultime creazioni</h4>
-        <img src="img1.jpeg" alt="flower" className="img-fluid" ></img>
+        <img src={img1.jpeg} alt="flower" className="img-fluid" />
         </section>
         
         </div></div>
