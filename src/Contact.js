7ee2cc5b8f6775d66cfc99586f3726a3baaa5993
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-07-11 14:28:06 +0200

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -8,7 +8,6 @@ export default function Contact(){
             <h3>Contattami</h3>
             <section>
             <p id="mail"> petitevintagecrafts@gmail.com</p>
-            <a href="https://www.instagram.com/petitevintagecrafts/?hl=es">Seguimi su Instagram!</a>
                 </section>
         </div>
     );
